99151828ff654a059767b1e0a69c22a09759d5a1
Farzaneh Fallahpour <farzanehhh.fl@gmail.com>
2021-11-13 10:28:51 +0330

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import SignIn from "./Page/Sign-in";
+import SignUp from "./Page/Sign-up";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import "antd/dist/antd.css";
 
 function App() {
+
+  // const [user, setUser] = useState()
+
+  // const getUser = () => {
+
+  // };
+
+  const dataAccount = (data) => {
+    // setUser((prevData) => {
+    //   console.log(prevData, data);
+    //   return [data, ...prevData];
+    console.log(data)
+    // });
+  };
+
+ 
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Switch>
+          <Route path="/" exact>
+            <SignIn dataAcount={dataAccount} />
+          </Route>
+          <Route path="/signup">
+            <SignUp />
+          </Route>
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ src/Page/Sign-in.js
@@ -0,0 +1,88 @@
+import { Link } from "react-router-dom";
+import { Form, Input, Button, Checkbox } from "antd";
+import { UserOutlined, LockOutlined } from "@ant-design/icons";
+import Card from "../component/Card";
+import "./Sign-in.css";
+
+const SignIn = (props) => {
+  const [form] = Form.useForm();
+  const submitHandler = (e) => {
+    e.preventDefault();
+  };
+  const onFinish = (values) => {
+    console.log("Received values of form: ", values);
+    form.resetFields();
+    props.dataAcount(values);
+  };
+
+  return (
+    <Card className="Card">
+      <h1>Blog</h1>
+      <Form
+        name="normal_login"
+        className="login-form"
+        initialValues={{
+          remember: true,
+        }}
+        form={form}
+        onFinish={onFinish}
+        onSubmit={submitHandler}
+      >
+        <Form.Item
+          name="username"
+          rules={[
+            {
+              required: true,
+              message: "Please input your Username!",
+            },
+          ]}
+        >
+          <Input
+            prefix={<UserOutlined className="site-form-item-icon" />}
+            placeholder="Username"
+          />
+        </Form.Item>
+        <Form.Item
+          name="password"
+          rules={[
+            {
+              required: true,
+              message: "Please input your Password!",
+            },
+          ]}
+        >
+          <Input
+            prefix={<LockOutlined className="site-form-item-icon" />}
+            type="password"
+            placeholder="Password"
+          />
+        </Form.Item>
+        <Form.Item>
+          <Form.Item name="remember" valuePropName="checked" noStyle>
+            <Checkbox>Remember me</Checkbox>
+          </Form.Item>
+
+          <a className="login-form-forgot" href="">
+            Forgot password
+          </a>
+        </Form.Item>
+
+        <Form.Item>
+          <Button
+            type="primary"
+            htmlType="submit"
+            className="login-form-button"
+          >
+            Log in
+          </Button>
+          <div className="orLine">OR</div>
+          <div>
+            <Link to='/signup'>Regester now!</Link>
+          </div>
+        </Form.Item>
+      </Form>
+    </Card>
+  );
+};
+
+export default SignIn;
